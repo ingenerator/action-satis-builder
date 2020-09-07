7183ece17b45ac3bee0ec80441838c213b7ab5de
acoulton <andrew@ingenerator.com>
2020-09-07 13:30:25 +0100

    Support using / exposing the composer cache between satis runs

--- builder/build-package-repo.sh
+++ builder/build-package-repo.sh
@@ -6,6 +6,8 @@ echo "Creating composer working directory with auth token"
 mkdir $COMPOSER_HOME
 echo  "{\"github-oauth\": {\"github.com\": \"$GITHUB_TOKEN\"}}" > $COMPOSER_HOME/auth.json
 
+export COMPOSER_CACHE_DIR="$GITHUB_WORKSPACE/$CACHE_DIR"
+
 echo "Compiling satis.json from dynamic package sources"
 "/repo-builder/vendor/bin/satisfy" \
   --repofile "$GITHUB_WORKSPACE/satis-explicit-packages.json" \
